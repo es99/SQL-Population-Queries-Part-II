3cd31fe8d66c6753aa735156627d2d62fee6c528
es99 <engels.franca@gmail.com>
2020-09-05 13:59:34 -0300

    projeto concluido

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ to retrieve interesting data and answer a set of specific questions.
 #### 1. How many entries in the database are from Africa?
 #### 2. What was the total population of Oceania in 2005?
 #### 3. What is the average population of countries in South America in 2003?
+#### 4. What country had the smallest population in 2007?
+#### 5. What is the average population of Poland during the time period covered by this dataset?
+#### 6. How many countries have the word "The" in their name?
+#### 7. What was the total population of each continent in 2010?
--- population_queries.sql
+++ population_queries.sql
@@ -14,3 +14,29 @@ SELECT countries.continent, ROUND(AVG(population_years.population), 2) AS media_
 FROM population_years
 JOIN countries ON population_years.country_id = countries.id
 WHERE countries.continent = 'South America' AND population_years.year = '2003';
+
+-- What country had the smallest population in 2007?
+SELECT countries.name, countries.continent,
+	population_years.year, MIN(population_years.population) AS menor_populacao
+FROM countries JOIN population_years
+ON population_years.country_id = countries.id
+WHERE population_years.year = '2007';
+
+-- What is the average population of Poland during the time period covered by this dataset?
+SELECT countries.name, countries.continent,
+	ROUND(AVG(population_years.population), 3) AS Media_populacao
+FROM countries JOIN population_years
+ON population_years.country_id = countries.id
+WHERE countries.name = 'Poland';
+
+-- How many countries have the word "The" in theis names?
+SELECT COUNT(*) AS The_in_their_names
+FROM countries
+WHERE name LIKE '% The%';
+
+-- What was the total population of each continent in 2010?
+SELECT countries.continent, population_years.year, ROUND(SUM(population_years.population)) AS Soma_da_populacao
+FROM countries JOIN population_years
+ON population_years.country_id = countries.id
+WHERE population_years.year = '2010'
+GROUP BY countries.continent;
